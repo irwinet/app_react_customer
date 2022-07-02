5c18a8436e20e68eaf16d790ae2d74cbe00a15bc
irwinet <irwinestradatorres@gmail.com>
2022-07-02 14:39:58 -0500

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import { Component } from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/customers">Customers</Link><br />
+          <Link to="/customers/1">Customer 1</Link>
+        </div>
+      </Router>
+    )
+  };
 }
 
 export default App;
