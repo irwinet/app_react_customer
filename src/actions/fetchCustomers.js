fed5bc4c716b00e671d879504e23bfd7bfca7135
irwinet <irwinestradatorres@gmail.com>
2022-07-12 19:46:54 -0500

    Add redux-promise

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,22 +1,8 @@
 import { FETCH_CUSTOMERS } from './../constants'
 import { createAction } from 'redux-actions'
 
-const customers = [
-    {
-        "dni": "76173874",
-        "name": "Irwin Estrada",
-        "age": 28
-    },
-    {
-        "dni": "11111111",
-        "name": "Qwerty Qwerty",
-        "age": 30
-    },
-    {
-        "dni": "22222222",
-        "name": "Azerty Azerty",
-        "age": 35
-    }
-]
+const url = 'http://localhost:3001/customers';
 
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
+const apiFetchCustomers = () => fetch(url).then(v => v.json())
+
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiFetchCustomers);
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import { compose, createStore } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise'
 import reducers from './../reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // const reducers = state => state;
 
-export const store = createStore(reducers, {}, composeEnhancers());
+export const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(promiseMiddleware)));
