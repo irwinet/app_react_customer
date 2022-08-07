a9a99d05623f8a28943dbbcd7044784b1bc7d24b
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-06 19:05:36 -0500

    Validation Globals - reduxForm

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -13,13 +13,24 @@ function withRouter(Component) {
     return ComponentWithRouter
 }
 
-const isRequired = value => (
-    !value && "Este campo es requerido"
-)
+// const isRequired = value => (
+//     !value && "Este campo es requerido"
+// )
 
 const isNumber = value => (
     isNaN(Number(value)) && "El campo debe ser un número"
 )
+
+const validate = values => {
+    const error = {};
+    if(!values.name){
+        error.name = "El campo nombre es requerido";
+    }
+    if(!values.dni){
+        error.dni = "El campo dni es requerido";
+    }
+    return error;
+}
 
 const MyField = ({input, meta, type, label, name}) => (
     <div>
@@ -40,13 +51,12 @@ const CustomerEdit = ({ name, dni, age }) => {
                     name='name' 
                     component={MyField}
                     type="text"
-                    validate={isRequired}
                     label="Nombre"></Field>        
                 <Field 
                     name='dni' 
                     component={MyField} 
                     type="text"
-                    validate={[isRequired, isNumber]}
+                    validate={isNumber}
                     label="Dni"></Field>        
                 <Field 
                     name='age' 
@@ -65,7 +75,11 @@ CustomerEdit.propTypes = {
     age: PropTypes.number
 }
 
-const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit)
+const CustomerEditForm = reduxForm(
+    { 
+        form: 'CustomerEdit',
+        validate 
+    })(CustomerEdit)
 
 export default compose(
     withRouter,              // <-- injects a params prop
