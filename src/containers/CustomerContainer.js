3a48310d53efb333cbec0b9ade073d50afcf4329
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-07 19:32:14 -0500

    Add reducer UPDATE_CUSTOMER

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -25,7 +25,7 @@ function withRouter(Component) {
 class CustomerContainer extends Component {
 
     componentDidMount(){
-        if(!this.props.customer.length === 0){
+        if(!this.props.customer){
             this.props.fetchCustomers();
         }
     }
